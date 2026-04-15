16df5903de4234afa5d811abd3590c8a4e43e750
hankli29 <hankli3000@gmail.com>
2026-04-15 22:49:23 +0000

    consumer solution

---
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,46 @@
+import sys
+import pika
+import os
+
+from consumer_interface import mqConsumerInterface
+
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(
+        self, binding_key: str, exchange_name: str, queue_name: str
+    ) -> None:
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+    
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        conParams = pika.URLParameters(os.environ['AMQP_URL'])
+        self.connection = pika.BlockingConnection(parameters=conParams)
+
+        self.channel = connection.channel()
+
+        self.channel.queue_declare(queue='Test')
+
+        self.channel.exchange_declare(exchange=self.exchange_name)
+        self.channel.queue_bind(queue=self.queue_name, exchange=self.exchange_name, routing_key=self.binding_key)
+
+    def on_message_callback(
+        self, channel, method_frame, header_frame, body
+    ) -> None:
+
+        self.channel.basic_ack(method_frame.delivery_tag, False)
+        print(body)
+        self.connection.close()
+    
+    def startConsuming(self) -> None:
+        print(" [*] Waiting for messages. To exit press CTRL+C")
+        self.channel.basic_consume(
+            self.queue_name, startConsuming, auto_ack=False
+        )
+
+    def __del__(self) -> None:
+        print("Closing RMQ connection on destruction")
+        self.channel.close()
+        self.connection.close()
